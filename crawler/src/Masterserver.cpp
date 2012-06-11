78c83881f34b54f039d886bbc37f93d72e38f78b
atomy <atomy@jackinpoint.net>
2012-06-11 16:44:01 +0200

    getting rid of old files

--- crawler/src/Masterserver.cpp
+++
@@ -1,9 +0,0 @@
-#include "const.h"
-#include <stdio.h>
-#include "Masterserver.h"
-#include <stdlib.h>
-
-Masterserver::Masterserver( servAddr sAddr )
-{
-	m_stServaddr = sAddr;
-}
--- crawler/src/PerformanceStats.cpp
+++
@@ -1,9 +0,0 @@
-#include "const.h"
-#include "PerformanceStats.h"
-
-using namespace std;
-
-PerformanceStats::PerformanceStats( int iMaxRetries ) : m_iMaxRetries( iMaxRetries )
-{
-    m_iCurrentRetries = 0;
-}
